a0daa3101d0d8899c1e73fcc1979af324fa753a9
kix6lj <kix6@outlook.com>
2024-12-20 23:02:38 +0800

    add minisat header

--- scheduler.cpp
+++ scheduler.cpp
@@ -1,6 +1,7 @@
 #include "common.h"
 #include <queue>
 #include <algorithm>
+#include <minisat/core/Solver.h>
 using namespace std;
 
 void schedule(DFG *dfg, const vector<Op*> &ops, double clock_period) {
